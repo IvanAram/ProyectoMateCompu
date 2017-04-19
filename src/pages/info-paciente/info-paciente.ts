1d54c7570df4aafd86564f2cccce416cc8242111
IvanAram <a01022584@itesm.mx>
2017-04-19 13:58:54 -0500

    Project

--- src/pages/info-paciente/info-paciente.ts
+++ src/pages/info-paciente/info-paciente.ts
@@ -6,21 +6,24 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'info-paciente.html'
 })
 export class InfoPacientePage {
-	paciente: any;
-	isUrgent: boolean = false;
-	hasHistorial: boolean = false;
-	hasSignos: boolean = false;
+  paciente: any;
+  isUrgent: boolean = false;
+  hasHistorial: boolean = false;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-  	this.paciente = this.navParams.get('paciente');
-  	this.isUrgent = (this.paciente.status.slice(0,1) == 'U' ? true : false);
-  	this.hasHistorial = (this.paciente.status.slice(1,2) == 'H' ? true : false);
-  	if(this.isUrgent){
-  		this.hasSignos = (this.paciente.status.slice(1,2) == '1') ? true : false;
-  	} else{
-  		this.hasSignos = (this.paciente.status.slice(2,3) == '1') ? true : false;
-  	}
+    this.paciente = this.navParams.get('paciente');
+    this.isUrgent = (this.paciente.status.slice(0,1) == 'U' ? true : false);
+    this.hasHistorial = (this.paciente.status.slice(1,2) == 'H' ? true : false);
   }
+
+  /*
+    this.paciente.signosVitales = {
+      frecCard: "a",
+      frecResp: "b",
+      presion: "c",
+      temperatura: "d"
+    };
+  */
 
   ionViewDidLoad() {
   }
